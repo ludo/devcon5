6bfd7b04056def012d4123f8c11976bb8621f7a1
Michel de Graaf <michel@re-invention.nl>
2009-10-28 17:31:18 +0100

    fixed disk usage

--- lib/checks/diskspace.rb
+++ lib/checks/diskspace.rb
@@ -2,15 +2,26 @@
 require 'net/ssh'
 class	DiskspaceScript < CheckScript
 	
-	def execute		
-	  # will fetch the in use coulmn
-		#todo : ssh to server, execute command and return result
-		#{}"df -l | cut -c 48-49"		
-		usage = "9000"
-	#	Net::SSH.start(address, "root") do |ssh|
-		  #output = ssh.exec!("df -l | cut -c 48-49")      
-  # end
-		"critical"
+	def execute	
+	  begin 	
+      	  # will fetch the in use coulmn
+      		#todo : ssh to server, execute command and return result
+      		#{}"df -l | cut -c 48-49"	
+      	 output = ""	
+      	 Net::SSH.start(address, "root") do |ssh|
+      		  output = ssh.exec!("df -l | cut -c 48-49")      
+         end
+         usage = "90000"
+         usage = output.split("\n").last.to_i unless output.blank?
+         return "critical" if usage.to_i >= @boundary_critical.to_i
+         return "warning" if usage.to_i >= @boundary_warning.to_i
+         return "normal" if usage.to_i < @boundary_normal.to_i        
+         
+   
+  rescue  Exception => e
+    puts "Exception while trying to get disk usage on host #{address} #{e.insect}"    
+  end
+
 	end
 end
 	
